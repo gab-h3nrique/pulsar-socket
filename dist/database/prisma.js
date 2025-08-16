9cc7a5aa6479e307201d93e958fbc991cf651f5d
gab Henrique <gabrielbielrique10@gmail.com>
2025-08-16 20:28:47 -0300

    fixing prisma generated

--- dist/database/prisma.js
+++ dist/database/prisma.js
@@ -5,3 +5,19 @@ const prisma = global.prisma || new prisma_1.PrismaClient();
 if (process.env.NODE_ENV === "development")
     global.prisma = prisma;
 exports.default = prisma;
+// import { PrismaClient } from './generated/prisma'
+// const prisma = new PrismaClient()
+// async function main() {
+// }
+// main()
+// .then(async () => await prisma.$disconnect())
+// .catch(async (e) => {
+//         console.error(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+// })
+// import { PrismaClient } from './generated/prisma'
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+// const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['error', 'warn'] })
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// export default prisma
